55250f007d53e632dc45f1169fca349963f12953
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-04-10 18:27:25 -0400

    Created model

--- train/train.py
+++ train/train.py
@@ -1,13 +1,15 @@
 import requests, zipfile, io, os, pickle
 import numpy as np
+import tensorflow as tf
+from keras import layers, models
 
 URL = "https://archive.ics.uci.edu/static/public/240/human+activity+recognition+using+smartphones.zip"
 DATA_PATH = os.path.join("dataset", "uci_har")
 
+# Extracting features from the dataset
 def extract_features(path): 
-    file_names = os.listdir(path)
     res = []
-    for name in file_names:
+    for name in os.listdir(path):
         feature = np.loadtxt(os.path.join(path, name))
         res.append(feature)
     
@@ -32,10 +34,10 @@ train_path = os.path.join(DATA_PATH, "UCI HAR Dataset", "train")
 test_path = os.path.join(DATA_PATH, "UCI HAR Dataset", "test")
 
 x_train = extract_features(os.path.join(train_path, "Inertial Signals"))
-y_train = np.loadtxt(os.path.join(train_path, "y_train.txt"))
+y_train = tf.one_hot(np.loadtxt(os.path.join(train_path, "y_train.txt")) - 1, depth=6)
 
 x_test = extract_features(os.path.join(test_path, "Inertial Signals"))
-y_test = np.loadtxt(os.path.join(test_path, "y_test.txt"))
+y_test = tf.one_hot(np.loadtxt(os.path.join(test_path, "y_test.txt")) - 1, depth=6)
 
 print("\nTraining Set:")
 print(f"x_train: {x_train.shape}")
@@ -45,8 +47,22 @@ print("Test Set:")
 print(f"x_test: {x_test.shape}")
 print(f"y_test: {y_test.shape}\n")
 
+print(f"TensorFlow version: {tf.__version__}\n")
 
+# Make model folder
+if not os.path.isdir("model"):
+    os.mkdir("model")
 
+model = models.Sequential([
+    layers.Input((x_train.shape[1], x_train.shape[2])),
+    layers.LSTM(64),
+    layers.Dropout(rate=0.2),
+    layers.Dense(128, activation="relu"),
+    layers.Dense(6, activation="softmax")
+])
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+history = model.fit(x_train, y_train, epochs=100, batch_size=32, validation_data=(x_test, y_test))
 
 
 
