d434c7be47bccaf407951d949a34bf14b96419b6
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-04-29 01:43:43 -0400

    Reorganized structure of items

--- src/website/app/src/components/Information.jsx
+++ src/website/app/src/components/Information.jsx
@@ -1,11 +1,11 @@
 import '../App.css'
 import Repetition from './Repetition';
-export default function Information({curl, raise, press}) {
+export default function Information({curl, press, raise}) {
     return (
         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', marginBottom: '10vh'}}>
             <Repetition exercise={"Curls"} count={curl}></Repetition>
-            <Repetition exercise={"Raises"} count={raise}></Repetition>
             <Repetition exercise={"Presses"} count={press}></Repetition>
+            <Repetition exercise={"Raises"} count={raise}></Repetition>
         </div>
     );
 }
